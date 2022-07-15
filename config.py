8c141cef0d2c408205ef58092f96f68db4605ba6
Mikael de Verdier <mikael.deverdier@gmail.com>
2022-07-15 14:06:08 +0200

    Small changes

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ residual_layer = {"amount": 5, "filter_amount": 75, "kernel_size": (4, 4)}
 dense_value_head = 64
 
 # Self-play
-game_amount_self_play = 50
-position_amount = 2500
+game_amount_self_play = 15
+position_amount = 1000
 MCTSSims = 25
 depth = 1
 turns_until_tau = 10
 
 # Retraining network
-training_iterations = 10
+training_iterations = 4
 batch_size = 16
 epochs = 1
 validation_split = 0.3
@@ -28,11 +28,11 @@ lr = 0.1
 momentum = 0.9
 
 # Evaluating network
-game_amount_evaluation = 25
+game_amount_evaluation = 5
 winning_threshold = 1.2
 
 # Play-test
 game_amount_play_test = 5
 
 # General
-save_folder = ""
+save_folder = "save_folder/"
