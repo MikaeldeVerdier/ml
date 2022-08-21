05fca4389e1ee9150f4a40b294025d99a29354aa
Mikael de Verdier <mikael.deverdier@gmail.com>
2022-08-21 14:41:19 +0200

    changed nn architechture

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 LOOP_ITERATIONS = 50
 
 # Network architecture
-CONVOLUTIONAL_LAYER = {"filter_amount": 75, "kernel_size": (4, 4)}
-RESIDUAL_LAYER = {"amount": 10, "filter_amount": 75, "kernel_size": (4, 4)}
+CONVOLUTIONAL_LAYER = {"filter_amount": 75, "kernel_size": (3, 3)}
+RESIDUAL_LAYER = {"amount": 20, "filter_amount": 75, "kernel_size": (3, 3)}
 DENSE_VALUE_HEAD = 20
 USE_BIAS = False
 
