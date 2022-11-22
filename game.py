corrected j_clip with masked softmax

game.py
@@ -8,8 +8,8 @@ REPLACE_CARDS = 3
 
 def generate_game_states(history, t):
     data = history[:t + 1]
-    game_states = ({"s": None},) * (config.DEPTH - len(data)) + tuple(data)[-config.DEPTH:]
-    game_states = tuple([game_state["s"] for game_state in game_states])
+    game_states = ({"state": None},) * (config.DEPTH - len(data)) + tuple(data)[-config.DEPTH:]
+    game_states = tuple([game_state["state"] for game_state in game_states])
 
     return game_states
 
@@ -72,7 +72,7 @@ def check_index(board, index, checking_index, checking_func, multiplier_func, mu
 def get_legal_moves(game_state):  # , all_moves):
     if not len(np.where(game_state.s != 0)[0]): return list(range(np.prod(GAME_DIMENSIONS)))
 
-    if game_state.replace_card: return list(range(np.prod(GAME_DIMENSIONS))) + [25]
+    if game_state.replace_card: return list(range(MOVE_AMOUNT))
 
     legal_moves = []
 
@@ -84,7 +84,7 @@ def get_legal_moves(game_state):  # , all_moves):
                     if check_index(game_state.s, index, checking_index, lambda x: x == 0, func, multiplier):
                         legal_moves.append(checking_index)
 
-    return legal_moves
+    return list(set(legal_moves))  # Prob can be more efficient
 
 
 def get_card(value):

main.py
@@ -53,13 +53,13 @@ def play(players, games, training=False):
             outcome = None
             while outcome is None:
                 if turn == config.TURNS_UNTIL_TAU: tau = 1e-2
-                storage.append({"s": player.mcts})
+                storage.append({"state": player.mcts})
 
-                a, pi_a, y_a, v = player.play_turn(storage, tau)
+                action, pi_action, y_a, value = player.play_turn(storage, tau)
 
                 if training:
-                    for i, var in enumerate(["a", "pi_a", "logit_a", "V_s"]):
-                        storage[-1][var] = [a, pi_a, y_a, v][i]
+                    for i, var in enumerate(["action", "pi_action", "logit_a", "value"]):
+                        storage[-1][var] = [action, pi_action, y_a, value][i]
                 outcome = game.check_game_over(player.mcts)
 
                 turn += 1
@@ -76,13 +76,13 @@ def play(players, games, training=False):
                 player.outcomes["average"] = (player.outcomes["average"] * (player.outcomes["length"] - 1) + int(outcome * 50)) / player.outcomes["length"]
             else:
                 for i, data in sorted(enumerate(storage), reverse=True):
-                    data["r"] = outcome
+                    data["reward"] = outcome
                     if i != len(storage) - 1:
                         data["delta"] = delta(storage, i)
 
                 for i, data in enumerate(storage[:-1]):
-                    data["Â"] = A(storage, i)
-                # storage[-1]["Â"] = outcome
+                    data["advantage"] = advantage(storage, i)
+                # storage[-1]["advantage"] = outcome
 
                 # away_from_full = config.POSITION_AMOUNT - len(loaded)
                 # if training_length > config.POSITION_AMOUNT / 25 or away_from_full and training_length >= away_from_full:
@@ -90,11 +90,14 @@ def play(players, games, training=False):
                 index = len(product)
 
                 for t, data in sorted(enumerate(storage), reverse=True):
+                    legal_moves = np.zeros(game.MOVE_AMOUNT)
+                    legal_moves[game.get_legal_moves(data["state"])] = 1
+
                     game_states = game.generate_game_states(storage, t)
 
                     if t != len(storage) - 1:
                         states = np.array(game.generate_nn_pass(game_states, True), dtype=object).tolist()
-                        for flip in states: product.append(np.array([flip, data["a"].item(), data["pi_a"], data["Â"], data["r"], product[-1][0]], dtype=object))  # [s, a, pi_a, Â, nn_value, nn_value_s+1, logits]
+                        for flip in states: product.append(np.array([flip, data["action"].item(), data["pi_action"], data["advantage"]] + list(legal_moves) + [data["reward"], product[-1][0]], dtype=object))  # [s, a, pi_action, advantage, nn_value, nn_value_s+1, logits]
                     else:
                         product.append([np.array(game.generate_nn_pass(game_states, False), dtype=object).tolist()])
                 
@@ -126,11 +129,11 @@ def play(players, games, training=False):
 def delta(data, t):
     if "delta" in data[t].keys():
         return data[t]["delta"]
-    delt = data[t]["r"] + config.GAMMA * data[t + 1]["V_s"] - data[t]["V_s"]
+    delt = data[t]["reward"] + config.GAMMA * data[t + 1]["value"] - data[t]["value"]
     return delt
 
 
-def A(data, t):
+def advantage(data, t):
     T = len(data)
     li = [(config.GAMMA * config.LAMBDA) ** i * delta(data, t + i) for i in range(T - t - 1)]
     return sum(li)
@@ -157,11 +160,11 @@ def retrain_network(agent):
                 y[head].append([])
             # y["value_head"].append([])
             # y["policy_head"].append([])
-            for i, var in enumerate(position[1:5]):
-                y["policy_head" if i != 3 else "value_head"][-1].append(var)
+            for i, var in enumerate(position[1:(5 + game.MOVE_AMOUNT)]):
+                y["policy_head" if i != 3 + game.MOVE_AMOUNT else "value_head"][-1].append(var)
 
-            posses = [pos[5] for pos in positions]
-            y["value_head"][-1].append(posses.index(position[5]))
+            posses = [pos[-1] for pos in positions]
+            y["value_head"][-1].append(posses.index(position[-1]))
 
             for i, dim in enumerate(position[0]):
                 x[i].append(np.array(dim))
@@ -185,7 +188,7 @@ def retrain_network(agent):
     agent.nn.model.save(f"{config.SAVE_PATH}training/v.{agent.nn.version}")
     agent.nn.save_metrics("current_agent")
     agent.outcomes = {"average": 0, "length": 0}
-    agent.nn.plot_metrics(derivative_lines=True)
+    agent.nn.plot_metrics()
 
 
 def evaluate_network(agents):
@@ -198,7 +201,7 @@ def evaluate_network(agents):
         outcomes[i] = np.sum(player) / len(player)
 
     log(agents, outcomes)
-    agents[-1].nn.plot_outcomes(derivative_line=True)
+    agents[-1].nn.plot_outcomes()
 
     print(f"The results were: {outcomes}")
     if outcomes[1] > outcomes[0] * config.WINNING_THRESHOLD:

nn.py
@@ -98,19 +98,30 @@ class NeuralNetwork:
     @staticmethod
     def J_clip(y_true, y_pred):
         y_true = y_true[0]
-        y_pred = y_pred[0]
+        logits = y_pred[0]
 
         a = tf.cast(y_true[0], tf.int32)
-        pi_a = y_true[1]
-        Â = y_true[2]
+        pi_action = y_true[1]
+        advantage = y_true[2]
+        legal_moves = y_true[2:]
 
-        pi_theta = y_pred[a]
-        pi_theta_old = pi_a
-        r_theta = pi_theta / pi_theta_old
+        mask = tf.equal(legal_moves, [1])
 
-        L_cpi = r_theta * Â
-        # L_clip = min(r_theta, 1 + config.EPSILON if Â > 0 else 1 - config.EPSILON)
-        # J_clip = min(L_cpi, L_clip)
+        new = np.full(game.MOVE_AMOUNT, -100)
+        for i in range(len((new))):
+            if tf.get_static_value(mask[i]):
+                new[i] = logits[i]
+
+        odds = np.exp(new).astype(np.float64)
+        pi = odds / np.sum(odds)
+
+        pi_theta = tf.convert_to_tensor(pi)[a]
+        pi_theta_old = pi_action
+        r_theta = tf.cast(pi_theta, tf.float32) / pi_theta_old
+
+        L_cpi = r_theta * advantage
+        L_clip = tf.math.minimum(r_theta, 1 + config.EPSILON if advantage > 0 else 1 - config.EPSILON) * advantage
+        J_clip = tf.math.minimum(L_cpi, L_clip)  # Can be effectivised asf
         J_clip = L_cpi
 
         mask = tf.greater(y_pred, 0)