baaed2f5535bdae705169ceed3876e380deb4fe8
Mikael de Verdier <mikael.deverdier@gmail.com>
2022-11-30 23:49:57 +0100

    now does not have a drawn_card in excess depth

--- game.py
+++ game.py
@@ -55,7 +55,14 @@ def generate_nn_pass(game_states, mirror=False):
                 if dr[0] != 0: drawn_card[dr[0] - 1] = 1
                 nn_pass[-1][2].append(drawn_card.tolist())
 
-                if game_states[-depth - 1]: game_state = game_states[-depth - 1]
+                if depth != config.DEPTH -1:
+                    if game_states[-depth - 2]: game_state = game_states[-depth - 2]
+                    else:
+                        for _ in range(config.DEPTH - depth - 1):
+                            nn_pass[-1][0].append(np.zeros((5, 5, 52)))
+                            nn_pass[-1][1].append(np.ones((52,)))
+                            nn_pass[-1][2].append(np.zeros((52,)))
+                        break
             
             nn_pass[-1] = [np.array(np.moveaxis(dim, 0, -1)).tolist() for dim in nn_pass[-1]]
             # nn_pass[-1] = [np.array(np.moveaxis(dim, 0, -1), dtype=np.int32).tolist() for dim in nn_pass[-1]]
