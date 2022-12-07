fixed advantage calculation

main.py
@@ -56,13 +56,13 @@ def play(players, games, training=False):
                 if turn == config.TURNS_UNTIL_TAU: tau = 1e-2
                 storage.append({"state": player.mcts})
 
-                action, pi_action, y_a, value = player.play_turn(storage, tau)
+                action, pi_action, value = player.play_turn(storage, tau)
 
                 outcome = game.check_game_over(player.mcts)
 
                 if training:
-                    for i, var in enumerate(["action", "pi_action", "logit_a", "value", "reward"]):
-                        storage[-1][var] = [action, pi_action, y_a, value, 0.0][i]
+                    for i, var in enumerate(["action", "pi_action", "value", "reward"]):
+                        storage[-1][var] = [action, pi_action, value, 0.0][i]
 
                 turn += 1
             starts *= -1
@@ -73,7 +73,7 @@ def play(players, games, training=False):
                 print(f"Amount of games played is now: {game_count} ({player.get_name()})\n")
 
             if not training:
-                outcome = int(outcome * 2)
+                outcome = int(outcome * 50)
                 outcomes[i].append(outcome)
                 player.outcomes["length"] += 1
                 player.outcomes["average"] = (player.outcomes["average"] * (player.outcomes["length"] - 1) + outcome) / player.outcomes["length"]
@@ -86,7 +86,7 @@ def play(players, games, training=False):
                     data["delta"] = delta(storage, i)
 
                 for t, data in sorted(enumerate(storage[:-1]), reverse=True):
-                    data["advantage"] = advantage(storage, i)
+                    data["advantage"] = advantage(storage, t)
 
                     legal_moves = np.zeros(game.MOVE_AMOUNT)
                     legal_moves[game.get_legal_moves(data["state"])] = 1

nn.py
@@ -126,7 +126,7 @@ class NeuralNetwork:
 
         mask = tf.greater(logits, 0)
         masked = tf.boolean_mask(logits, mask)
-        S_pi = -tf.math.reduce_sum(masked * tf.math.log(masked))
+        S_pi = -tf.math.reduce_sum(masked * tf.math.log(masked)) * 0.5
 
         loss = -J_clip - S_pi
 

player.py
@@ -47,7 +47,7 @@ class Agent():
 
     def play_turn(self, history, tau):
         game_states = game.generate_game_states(history, len(history) - 1)
-        (nn_value, probs, logits) = self.nn.get_preds(game_states)
+        (nn_value, probs, _) = self.nn.get_preds(game_states)
 
         action = self.choose_action(probs, tau)
 
@@ -55,7 +55,7 @@ class Agent():
 
         # self.print_move(self.mcts, probs, action, nn_value)
 
-        return action, probs[action], logits[action], nn_value
+        return action, probs[action], nn_value
 
     def getAV(self, tau):
         pi = np.zeros(game.MOVE_AMOUNT)