59ee8d43c376da913650cefa2be7090b4f173c65
Mikael de Verdier <mikael.deverdier@gmail.com>
2023-04-23 18:05:17 +0200

    last commit was actually mirroring added too

--- funcs.py
+++ funcs.py
@@ -107,11 +107,6 @@ def score_row(row):
 	return sum_score
 
 
-@cache()
-def change_suit(card, suit_change, deck_length):
-	return 
-
-
 @cache(10000)
 def format_state(board):
 	shape = (environment.NN_INPUT_DIMENSIONS[0][-1],) + environment.NN_INPUT_DIMENSIONS[0][:-2]
