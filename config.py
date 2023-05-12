00df92e8d84f6115e7b389ca1b4c0a24d5ae0b1d
Mikael de Verdier <mikael.deverdier@gmail.com>
2023-05-12 23:18:58 +0200

    success in no_shuffle, shuffle re-added with 300m nn

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 from funcs import linear_wrapper_func
 
 # Main loop
-VERSION_AMOUNT = 1500
+VERSION_AMOUNT = 2000
 
 # Network architecture
 USE_BIAS = True
 
-DENSE_POSITION = [3200, 3000, 2500, 2048, 2048, 2048, 2048, 2048, 2048, 1024, 512]
+DENSE_POSITION = [288, 512, 1024, 2048, 4092, 8192, 8192, 8192, 4092, 2048, 1024, 512]
 
-DENSE_DECK = [64, 128, 128, 128]
+DENSE_DECK = [64, 128, 256, 256, 256]
 
-DENSE_DRAWN_CARD = [64, 128, 128, 128]
+DENSE_DRAWN_CARD = [64, 128, 256, 256, 256]
 
-DENSE_POLICY_HEAD = [768, 1024, 1024, 1024, 512, 512, 512, 256, 128, 64, 32, 32]
+DENSE_POLICY_HEAD = [1024, 2048, 4092, 4092, 4092, 4092, 2048, 1024, 512, 256, 128, 64, 32, 16, 16]
 
 # Self-play
 GAME_AMOUNT_SELF_PLAY = 48
@@ -28,7 +28,7 @@ EPOCHS = 1  # Amount of times batch is looped over
 GAMMA = 0.8  # Discounting factor for future rewards when calculating targets
 VERSION_OFFSET = 25  # Reciprocal of frequency of target neural network copying main neural network
 SAVING_FREQUENCY = 250  # Reciprocal of frequency of saving progress
-MODEL_CHECKPOINT_FREQUENCY = 750  # Reciprocal of frequency of saving checkpoint models
+MODEL_CHECKPOINT_FREQUENCY = 500  # Reciprocal of frequency of saving checkpoint models
 VALIDATION_SPLIT = 0.2  # Share of data used for validation
 REG_CONST = 1e-4  # L2 Regularization hyperparameter
 learning_rate = linear_wrapper_func(1e-4, 1e-6, VERSION_AMOUNT * 0.9, use_cache=False)
@@ -38,7 +38,7 @@ GAME_AMOUNT_EVALUATION = 100
 EVALUATION_FREQUENCY = 25  # Reciprocal of frequency of evaluating model
 
 # Play versions
-GAME_AMOUNT_PLAY_VERSIONS = 10000
+GAME_AMOUNT_PLAY_VERSIONS = 100000
 
 # Play-test
 GAME_AMOUNT_PLAY_TEST = 4
